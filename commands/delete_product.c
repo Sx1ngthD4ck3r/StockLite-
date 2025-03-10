f08cd26faa029ac939dc9bb64c61789693c17348
Sx!ngth <Sx1ngth@proton.me>
2025-03-10 10:53:32 -0500

    Update delete_product.c

--- commands/delete_product.c
+++ commands/delete_product.c
@@ -4,28 +4,34 @@ void delete_product()
 {
     int rc;
     char *errmsg = 0;
-
     sqlite3 *db;
 
     rc = sqlite3_open("output/inventary.db", &db);
-
     if (rc != SQLITE_OK)
     {
-        fprintf(stderr, "Error al abrir la base de datos\n", sqlite3_errmsg(db));
+        fprintf(stderr, "Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db); // Cerrar para evitar fugas de memoria
+        return;
     }
 
     int id_delete;
-
     printf("\n[delete-id] >> ");
+    scanf("%i", &id_delete);
 
     const char *sql = "DELETE FROM tienda WHERE id = ?";
-
     sqlite3_stmt *stmt;
 
-    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    scanf("%i", &id_delete);
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Error al preparar la consulta: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return;
+    }
+
     sqlite3_bind_int(stmt, 1, id_delete);
     rc = sqlite3_step(stmt);
+
     if (rc != SQLITE_DONE)
     {
         fprintf(stderr, "Error al ejecutar la consulta: %s\n", sqlite3_errmsg(db));
